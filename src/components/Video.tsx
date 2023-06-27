82849917ebc1cc64162c73352b70fe053f6cde42
Italo <italodopeey@gmail.com>
2023-06-27 00:01:43 -0300

    feat: autoplay

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,11 +1,15 @@
 import ReactPlayer from 'react-player'
 import { UseAppSelector } from '../store'
+import { useDispatch } from 'react-redux'
+import { next } from '../store/slices/player'
 
 interface VideoProps {
   videoID: string
 }
 
 export default function VideoPlayer() {
+  const dispatch = useDispatch()
+
   const lesson = UseAppSelector((state) => {
     const { currentLessonIndex, currentModuleIndex } = state.player
 
@@ -17,10 +21,15 @@ export default function VideoPlayer() {
     return currentLesson
   })
 
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
+        onEnded={handlePlayNext}
         height="470px"
         controls
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
